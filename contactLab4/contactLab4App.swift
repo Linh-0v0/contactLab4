e757e2d402b89f7a2e7f72be664272c11f383efd
Vu Bui Khanh Linh <vlinh271001@icloud.com>
2022-07-19 14:16:12 +0700

    Initial Commit

---
+++ contactLab4/contactLab4App.swift
@@ -0,0 +1,17 @@
+//
+//  contactLab4App.swift
+//  contactLab4
+//
+//  Created by Vu Bui Khanh Linh on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct contactLab4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
